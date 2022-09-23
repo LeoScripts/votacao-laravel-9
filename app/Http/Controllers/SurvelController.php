34a4bfc31e9838b5ec8044c0027c4939c137092d
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-09-22 22:25:43 -0300

    feat: implemeted routes resource in laravel

---
+++ app/Http/Controllers/SurvelController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSurvelRequest;
+use App\Http\Requests\UpdateSurvelRequest;
+use App\Models\Survel;
+
+class SurvelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSurvelRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSurvelRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Survel  $survel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Survel $survel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Survel  $survel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Survel $survel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSurvelRequest  $request
+     * @param  \App\Models\Survel  $survel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSurvelRequest $request, Survel $survel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Survel  $survel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Survel $survel)
+    {
+        //
+    }
+}
