0c6806c27588cea4a665d490f12e53c8e30524ff
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-09-22 23:02:41 -0300

    test: rendering_all_survey

---
+++ tests/Feature/Survey/SurveyTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Survey;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SurveyTest extends TestCase
+{
+
+    // aqui estou usando a notation @test
+
+    /**
+     * A basic feature test example.
+     *
+     * @test
+     */
+    public function rendering_all_survey()
+    {
+        $response = $this->get('/survey');
+
+        $response->assertStatus(200);
+    }
+}
