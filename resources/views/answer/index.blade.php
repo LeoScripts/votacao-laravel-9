ea2387f711e501c35cef517b6111298e1a63c4d9
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-09-27 08:53:32 -0300

    feat: change style answers index

--- resources/views/answer/index.blade.php
+++ resources/views/answer/index.blade.php
@@ -7,14 +7,12 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div style=" display:flex; justify-content:end">
+                <a style="background: rgb(0, 234, 255);margin:1rem; padding:0.5rem 1rem; border-radius:0.5rem;text-align:center;max-width:10rem;" href="{{ route('answer.create') }}">Nova Resposta</a>
+            </div>
+
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @auth
-                        <b>{{Auth::user()->name}}</b>  aqui estao as respostas <br>
-                    @endauth
-
-                    <a style="background: rgb(0, 234, 255)" href="{{ route('answer.create') }}">Nova Resposta</a>
-
                     @foreach($questions as $qItem)
 
                         <div style="background: #f693; margin:0.25rem; padding:0.5rem; border-radius:0.5rem;">
@@ -22,7 +20,7 @@
 
                             <div style="display: flex;">
                                 @foreach ($qItem->answer as $aItem)
-                                    <p style="background: #fff; margin:0.25rem; padding:0.5rem 1rem; border-radius:0.5rem; width:7rem; text-align:center;">{{$aItem->answer}}</p>
+                                    <p style="background: #fff; margin:0.25rem; padding:0.5rem 1rem; border-radius:0.5rem; max-width:15rem; text-align:center;">{{$aItem->answer}}</p>
                                 @endforeach
                             </div>
                         </div>
