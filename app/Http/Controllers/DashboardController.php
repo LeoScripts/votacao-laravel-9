d88f5bcc920e1755808d1d28675245e51622be59
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-10-02 20:24:35 -0300

    feat: implente send questions and answers for view dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Question;
 
 class DashboardController extends Controller
 {
+    public function __construct(Question $question)
+    {
+        $this->question = $question;
+    }
     public function index()
     {
-        $data = 'meus dados';
+        $data = $this->question->all();
         return view('dashboard', compact('data'));
     }
 }
