9a4b12059db6fcd5c061607396a98de6ca918dd7
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-10-13 14:19:23 -0300

    feat: modified styled of dasbord view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,12 +10,12 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     @foreach ($questions as $questionItem)
-                        <div class="p-2 m-2 bg-cyan-100">
+                        <div class="p-2 m-2 bg-cyan-500">
                            <h2 class="m-3">{{ $questionItem->question }}</h2>
                             <div class="flex gap-2">
                                 @foreach ($questionItem->answer as $answerItem)
                                     <div class="p-2 bg-slate-50 rounded-md">
-                                        {{ $answerItem->answer }} - {{ $answerItem->vote }}
+                                        {{ $answerItem->answer }}  <span class="rounded-full bg-yellow-400 p-1 relative left-3 top-[-18px]">{{ $answerItem->vote }}</span> 
 
                                         <form action="{{ route('answer.update', $answerItem->id)}}" method="post">
                                             @method('PATCH')
