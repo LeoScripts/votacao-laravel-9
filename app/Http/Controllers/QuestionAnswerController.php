28b47a41b76f415817f415b126741b1fde539b2d
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-11-06 16:15:41 -0300

    feat: implementation validation vote

--- app/Http/Controllers/QuestionAnswerController.php
+++ app/Http/Controllers/QuestionAnswerController.php
@@ -31,7 +31,14 @@ class QuestionAnswerController extends Controller
      */
     public function store(Request $request)
     {
-        // if(  )
+        $checkUserVote = $this->model->where('user_id', auth()->user()->id)->get();
+        // dd($userDb);
+
+        foreach($checkUserVote as $userVotes){
+            if($userVotes->question_id  == $request->question_id && $userVotes->user_id == $request->user_id) {
+                return  'Voce ja votou nesta inquete';
+            }
+        }
 
         $this->model->create($request->only('question_id', 'answer_id', 'user_id'));
         return redirect()->route('dashboard');
